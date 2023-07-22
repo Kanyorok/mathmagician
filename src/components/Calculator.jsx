e4fe52a87aab207f06becbee8faa6b6b091768c0
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-07-22 23:00:11 +0300

    Fix the loading state of the application from API

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import calculate from '../logic/calculate';
 import AnswerSection from './AnswerSection';
 import keysPress from '../logic/keysPress';
-import QuoteApi from './QuoteApi';
 
 export default function Calculator() {
   const [result, setResult] = useState({});
@@ -49,13 +48,8 @@ export default function Calculator() {
   ];
 
   return (
-    <div className="mx-auto flex justify-between mt-14 overflow-hidden mb-2 w-full">
-      <div className="flex items-center ml-10 justify-center rounded-md text-center w-2/6 bg-slate-600 border-solid border-black border-2">
-        <div className="bg-blue-400 shadow-2xl rounded-md p-6 text-purple-700 font-bold text-center text-2xl">
-          <QuoteApi />
-        </div>
-      </div>
-      <div className="grid grid-cols-4 w-2/6 grid-flow-row mr-10 bg-slate-600 border-solid border-black border-2">
+    <div className="mx-auto mt-14 overflow-hidden mr-10 mb-2 lg:w-4/6 md:w-3/6 sm:w-4/6">
+      <div className="grid grid-cols-4 grid-flow-row bg-slate-600 border-solid border-black border-2">
         <div className="col-span-4 text-white text-3xl text-right p-5 h-24 pt-16 pb-0 mb-2">
           <AnswerSection
             output={result.total ? result.total : ''}
--- src/components/QuoteApi.jsx
+++ src/components/QuoteApi.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 const apiKey = new Headers();
 apiKey.append('X-Api-Key', 'fi23b02FxA5RuBZcQMwheA==1pRuDF9DyRGAqaht');
-const apiSite = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
+const apiSite = 'https://api.api-ninjas.com/v/quotes?category=happiness';
 
 export default function QuoteApi() {
   const [quote, setQuote] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [quoteLoading, setQuoteLoading] = useState(false);
 
   useEffect(() => {
+    setQuoteLoading(true);
     const fetchQuotes = async () => {
       try {
         const response = await fetch(apiSite, {
@@ -17,12 +19,13 @@ export default function QuoteApi() {
         });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok.');
+          setErrorMessage('Network response was not ok.');
         }
 
         const fetchedQuotes = await response.json();
         setQuote(fetchedQuotes);
       } catch (error) {
+        setQuoteLoading(false);
         setErrorMessage(`Error fetching data: ${error.message}`);
       }
     };
@@ -30,30 +33,30 @@ export default function QuoteApi() {
     fetchQuotes();
   }, []);
 
-  if (errorMessage) {
-    return <h1>{errorMessage}</h1>;
-  }
-
   return (
     <div>
-      {quote.length > 0 ? (
-        <ul>
-          {quote.map((item) => (
-            <li key={`${item.quote}-${item.author}`}>
-              Quote:
-              <br />
-              {item.quote}
-              <br />
-              <br />
-              Author:
-              {' '}
-              {item.author}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Quotes</p>
-      )}
+      <div className="flex flex-grow-0 items-center ml-10 justify-center max-w-1/4 rounded-md text-center w-4/6 bg-slate-600 border-solid border-black border-2">
+        <div className="bg-blue-400 shadow-2xl rounded-md p-6 max-w-1/4 text-purple-700 font-bold text-center text-2xl">
+          {quoteLoading ? (
+            <ul>
+              {quote.map((item) => (
+                <li key={`${item.quote}-${item.author}`}>
+                  Quote:
+                  <br />
+                  {item.quote}
+                  <br />
+                  <br />
+                  Author:
+                  {' '}
+                  {item.author}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>{errorMessage}</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
